b6c81b446b0372897ae38baf56f66f99fce52066
simo-mac <sidrissi82@gmail.com>
2025-11-08 10:28:40 +0100

    fix cloudflare deployment

---
+++ .open-next/assets/_next/static/chunks/app/[locale]/page-454213505e7acd68.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61,961,405,853,53],{8255:function(e,n,s){Promise.resolve().then(s.bind(s,7867)),Promise.resolve().then(s.bind(s,1085))}},function(e){e.O(0,[85,971,117,744],function(){return e(e.s=8255)}),_N_E=e.O()}]);
---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_BASE_URL":"https://localhost:3000","NEXT_PUBLIC_PLAUSIBLE_DOMAIN":""};
+export const development = {"NEXT_PUBLIC_BASE_URL":"https://localhost:3000","NEXT_PUBLIC_PLAUSIBLE_DOMAIN":""};
+export const test = {"NEXT_PUBLIC_BASE_URL":"https://localhost:3000","NEXT_PUBLIC_PLAUSIBLE_DOMAIN":""};
